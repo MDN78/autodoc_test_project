6de3d3946e4d97b3cbf4f91032da41d322152707
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-03-04 19:40:27 +0300

    create structure of project

--- autodoc/pages/main_page.py
+++ autodoc/pages/main_page.py
@@ -1,4 +1,3 @@
-import allure
 from selene import browser, have, be
 # # from selene.core import command
 # # from utils.logger import step
@@ -7,17 +6,13 @@ from selene import browser, have, be
 #
 class MainPage:
 
-    # @step
-    @allure.step('Open maim page')
     def open(self):
         browser.open("/")
 
-    @allure.step('Search item')
     def search_item_by_tool_number(self, value):
         browser.element('#partNumberSearch').should(be.blank).send_keys(value)
         browser.element('.search-button').click()
         browser.element('.breadcrumbs-header').should(have.text(value))
 
-    @allure.step('Checking main title "Запчасти в интернет-магазине Автодок"')
     def main_page_should_have_visible_text(self):
         browser.element('.homepage-content__title').should(have.exact_text('Запчасти в интернет-магазине Автодок'))
