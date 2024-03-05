87b56ff3a6e7a6cef87c4e8191ee730256671305
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-03-05 13:37:11 +0300

    first working tests

--- autodoc/pages/main_page.py
+++ autodoc/pages/main_page.py
@@ -1,14 +1,47 @@
 import allure
 from selene import browser, have, be
-
+from autodoc.data.users import User
 from utils.logger import step
 
 
 class MainPage:
 
+    @step
     @allure.step("open browser")
     def open(self):
         browser.open("/")
+
+    @step
+    @allure.step('Authorization registered user')
+    def authorization_registered_user(self, user: User):
+        browser.element('#loginInfo').click()
+        browser.element('#Login').should(be.blank).send_keys(user.username)
+        browser.element('#Password').should(be.blank).send_keys(user.password)
+        browser.element('.icon.fa').click()
+        browser.element('#submit_logon_page').click()
+        browser.element('#user_info').should(have.exact_text(user.username))
+
+    @step
+    @allure.step('Authorization unregistered user')
+    def authorization_unregistered_user(self, user: User):
+        browser.element('#loginInfo').click()
+        browser.element('#Login').should(be.blank).send_keys(user.username)
+        browser.element('#Password').should(be.blank).send_keys(user.password)
+        browser.element('.icon.fa').click()
+        browser.element('#submit_logon_page').click()
+        browser.element('#errorMessage').should(have.exact_text('Не удалось авторизоваться.'))
+
+
+    # @step
+    # @allure.step('Authorization')
+    # def authorization(self, name, password):
+    #     browser.element('#loginInfo').click()
+    #     browser.element('#Login').should(be.blank).send_keys(name)
+    #     browser.element('#Password').should(be.blank).send_keys(password)
+    #     browser.element('.icon.fa').click()
+    #     browser.element('#submit_logon_page').click()
+    #     browser.element('#user_info').should(have.exact_text(name))
+
 
     @step
     @allure.step("Search")
---
+++ tests/test_autodoc_auth_user.py
@@ -0,0 +1,23 @@
+import os
+from autodoc.data.users import User
+from autodoc.pages.main_page import MainPage
+
+
+def test_authorization_registered_user():
+    main_page = MainPage()
+    main_page.open()
+    registered_user = User(
+        username=os.getenv('USER_LOGIN'),
+        password=os.getenv('USER_PASSWORD')
+    )
+    main_page.authorization_registered_user(registered_user)
+
+
+def test_authorization_unregistered_user():
+    main_page = MainPage()
+    main_page.open()
+    registered_user = User(
+        username=os.getenv('UNREGISTERED_USER_LOGIN'),
+        password=os.getenv('UNREGISTERED_USER_PASSWORD')
+    )
+    main_page.authorization_unregistered_user(registered_user)
