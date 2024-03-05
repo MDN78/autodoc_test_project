bbf7fb4374ca88ad1f8eb7af66c279746e291782
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-03-05 13:50:35 +0300

    minor updates - change username

--- tests/test_autodoc_unregistered_user.py
+++ tests/test_autodoc_unregistered_user.py
@@ -6,13 +6,13 @@ from autodoc.pages.main_page import MainPage
 def test_add_item_to_cart():
     pass
 
-@pytest.mark.skip
+
 def test_open_main_page():
     main_page = MainPage()
     main_page.open()
     main_page.search_item_by_tool_number('ZIC 132661')
 
-@pytest.mark.skip
+
 def test_main_page_should_have_exact_visible_text():
     main_page = MainPage()
     main_page.open()
